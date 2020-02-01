c3276711e96aa82f894e633835d6b95576d0d70c
hninthetstar <hninnthetmhuukhin@gmail.com>
2020-02-01 12:16:29 +0630

    List

---
+++ variable.py
@@ -0,0 +1,74 @@
+#variable
+print ("Hello World")
+
+>>> 2 + 2
+4
+>>> 50 - 5 * 6
+20
+>>> ( 50 - 5 * 6 ) / 4
+5.0
+>>> round( 5.0 )
+5
+>>> round (5.023434,3)
+5.023
+>>>
+
+
+int - integer - 1, 2, 3, ----> 308930 etc
+float -        - 1.0, 5.9, 6.0, 63.23, etc
+string -        - 'Hello', 'World', 'A', 'B',
+
+a = 1
+a (variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+volume = width * height
+volume
+
+print('spam egg')
+print('don\'t')
+print("doesn't")
+print("Yes")
+print("\"Yes,\" they said.")
+print('"Isn \'t,"" they said.')
+print('"Isn\'t," they said.')
+"'Hello World'"
+
+addition = 75 + 25
+subtraction = 204 - 204
+multiplication = 100 * 0.5
+division = 90 / 9
+power of 2 = 3 ** 2
+the remainder of the division = 10 % 3
+
+variable
+
+* or %
+5 * 3 + 2 % 3
+5 * 3 + 3 % 4
+6 * 9 - 4 * 5 % 2
+7 * 5 - 6 * 3 % 3
+6 * 5 - 7 * 4 / 4 % 2
+8 * 9 / 4 * 5 % 3 - 2
+
+a = 1
+a (varaible) = (assign) (value)
+
+width = 20
+height = 5*9
+volume = width * height
+
+>>> sale = 1500
+>>> tax = 5 / 100
+>>> total_tax = sale * tax
+>>> total_tax
+75.0
+>>> total_price = sale + tax
+>>> total_price
+1500.05
+>>> total_price = sale + total_tax
+>>> total_price
+1575.0
+>>> round(total_price)
+1575
