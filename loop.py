5e85c08ba3f80e050a4241de80eaacba1f30ff94
hninthetstar <hninnthetmhuukhin@gmail.com>
2020-02-29 12:12:18 +0630

    Fibonacci.py

---
+++ loop.py
@@ -0,0 +1,25 @@
+
+
+loop.py
+
+-while Loops
+-for Loops
+
+Condition it true, while loop execute a set of statements
+
+x = 1
+while x < 7:
+		print(x)
+		x += 1
+
+
+
+While loop require variable ready.
+
+x = 1
+while x < 7:
+		print(x)
+		if x == 5:
+			break
+		x += 1
+
